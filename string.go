d7b318be555e5da89c0d00c06e385e79cf941a29
flywithbug <369495368@qq.com>
2019-06-16 23:21:46 +0800

    a

--- string.go
+++ string.go
@@ -1,7 +1,6 @@
 package utils
 
 
-
 import (
 	"fmt"
 	"math/rand"
@@ -26,3 +25,32 @@ func RandString(n int, defaultCharsets ...string) string {
 
 	return result
 }
+
+func RandSMSString(n int, defaultCharsets ...string) string {
+	charset := Alphanumeric
+	if len(defaultCharsets) > 0 {
+		charset = defaultCharsets[0]
+	}
+
+	result, err := String(n, charset)
+	if err != nil {
+		fmt.Println("goutils RandString error:", err)
+		return Md5(strconv.Itoa(rand.Intn(999999)))
+	}
+
+	return result
+}
+
+//func ConvertInt2String(v interface{})string  {
+//	switch v.(type) {
+//	case int:
+//		return strconv.Itoa(int(v))
+//	case string:
+//		return string(v)
+//	case int64:
+//		return strconv.FormatInt(int64(v),10)
+//	default:
+//		return ""
+//	}
+//}
+
